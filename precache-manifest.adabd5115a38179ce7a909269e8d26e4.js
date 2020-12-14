f59248467c1890fb9319021a85276a5a6bb8861a
Saajan <saajan.joshi@sagarmatha.edu.np>
2020-12-14 23:26:27 +0545

    Updates

---
+++ precache-manifest.adabd5115a38179ce7a909269e8d26e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58766ccd8f481f2f6d764fb7947f8ae4",
+    "url": "/PortfolioSite/index.html"
+  },
+  {
+    "revision": "e248178b869763be1b3f",
+    "url": "/PortfolioSite/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "86273232b9a94d9ea5b6",
+    "url": "/PortfolioSite/static/js/2.23af08e9.chunk.js"
+  },
+  {
+    "revision": "e248178b869763be1b3f",
+    "url": "/PortfolioSite/static/js/main.93c9c514.chunk.js"
+  },
+  {
+    "revision": "686291a9438288583a77",
+    "url": "/PortfolioSite/static/js/runtime~main.bb0321c5.js"
+  }
+]);
