b4d96b79f4dce6eae99bed1534e845b7208b2bd3
Saajan <saajan.n.joshi@gmail.com>
2023-07-02 19:40:20 -0400

    removed project import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Sidebar from "./components/Sidebar";
 import Introduction from "./components/Introduction";
 import About from "./components/About";
-import Projects from "./components/Projects";
 import Timeline from "./components/Timeline";
 
 class App extends Component {
